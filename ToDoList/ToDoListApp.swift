1b846a06bab986eb20d1348fc3b7c299df65342a
Hoja Arzanesh <ha1797@rit.edu>
2024-06-14 16:15:06 -0400

    initialized branch

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Hoja Arzanesh on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
