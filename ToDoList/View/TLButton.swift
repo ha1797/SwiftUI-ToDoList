2ad53d8cdfe76fbd858efcb02db7d94a7b940b36
Hoja Arzanesh <ha1797@rit.edu>
2024-06-17 15:00:22 -0400

    added parameters (title, backgroundColor, action) to struct

--- ToDoList/View/TLButton.swift
+++ ToDoList/View/TLButton.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 struct TLButton: View {
+    let title: String
+    let backgroundColor: Color
+    let action: () -> Void
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    TLButton()
+    TLButton(title: "title", backgroundColor: .blue, action: {/* action */})
 }
--- ToDoList/ViewModel/LoginViewViewModel.swift
+++ ToDoList/ViewModel/LoginViewViewModel.swift
@@ -5,4 +5,9 @@
 //  Created by Hoja Arzanesh on 6/17/24.
 //
 
+import FirebaseAuth
 import Foundation
+
+class LoginViewViewModel: ObservableObject {
+    
+}
