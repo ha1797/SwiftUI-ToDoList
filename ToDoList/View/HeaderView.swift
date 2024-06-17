9077cda08a446305a88eed5a5d07b7e709850f07
Hoja Arzanesh <ha1797@rit.edu>
2024-06-17 09:16:00 -0400

    added necessary files to View group

---
+++ ToDoList/View/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  ToDoList
+//
+//  Created by Hoja Arzanesh on 6/17/24.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
---
+++ ToDoList/View/ToDoListItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoListItemView.swift
+//  ToDoList
+//
+//  Created by Hoja Arzanesh on 6/17/24.
+//
+
+import SwiftUI
+
+struct ToDoListItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoListItemView()
+}
