cc7670d77c0e0dfcf45e6774ec6e2212af9f57ba
Hoja Arzanesh <78914545+ha1797@users.noreply.github.com>
2024-06-18 09:24:00 -0400

    Header view (#2)
    * created groups Model, View, ViewModel, Other, and moved Assets and ToDoListApp files to Other group
    
    * added necessary files to Model group
    
    * added extensions file to Other group
    
    * added necessary files to ViewModel group
    
    * added necessary files to View group
    
    * added firebase package
    
    * changed orientation settings so only portrait mode is used, and changed app display name to 'To Do List'
    
    * added google service plist file to Other group
    
    * imported FirebaseCore to ToDoListApp swift file in Other group
    
    * added init() method to ToDoListApp initializing Firebase within the app
    
    * commented out ContentView() and replaced it with MainView()
    
    * added parameters to header view
    
    * added angled rectangle to header view
    
    * added text (title, subtitle) on top of angled rectangle

--- ToDoList/View/HeaderView.swift
+++ ToDoList/View/HeaderView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct HeaderView: View {
+    let title: String
+    let subTitle: String
+    let angle: Double
+    let backgroundColor: Color
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 0)
+                .foregroundColor(backgroundColor)
+                .rotationEffect(Angle(degrees: angle))
+            
+            VStack {
+                Text(title)
+                    .font(.system(size: 50))
+                    .foregroundStyle(Color.white)
+                    .bold()
+                Text(subTitle)
+                    .font(.system(size: 30))
+                    .foregroundStyle(Color.white)
+            }
+            .padding(.top, 30)
+        }
+        .frame(width: UIScreen.main.bounds.width*3, height: 350)
+        .offset(y: -150)
     }
 }
 
 #Preview {
-    HeaderView()
+    HeaderView(title: "Title", subTitle: "Subtitle", angle: 15, backgroundColor: .blue)
 }
