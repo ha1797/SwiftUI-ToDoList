c7d0568c231d52d92c887260ca87e427c664fd86
Hoja Arzanesh <ha1797@rit.edu>
2024-06-18 15:42:28 -0400

    added init function in MainViewViewModel to initialize the currentUserId to logged in user or empty string if not logged in

--- ToDoList/ViewModel/MainViewViewModel.swift
+++ ToDoList/ViewModel/MainViewViewModel.swift
@@ -13,5 +13,14 @@ class MainViewViewModel: ObservableObject {
     @Published var currentUserId: String = ""
     private var handler: AuthStateDidChangeListenerHandle?
     
+    init() {
+        self.handler = Auth.auth().addStateDidChangeListener(
+            { [weak self] _, user in
+                DispatchQueue.main.async {
+                    self?.currentUserId = user?.uid ?? ""
+                }
+            }
+        )
+    }
     
 }
