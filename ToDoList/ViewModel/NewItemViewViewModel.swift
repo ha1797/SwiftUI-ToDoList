a36a15de18e03e0b6a1541538c1bb64462500093
Hoja Arzanesh <ha1797@rit.edu>
2024-06-19 23:22:40 -0400

    add title, dueDate, showAlert properties
    title, dueDate is name and due date of new Task, and showAlert will determine if alert should be shown. Any changes will be notified to observers

--- ToDoList/ViewModel/NewItemViewViewModel.swift
+++ ToDoList/ViewModel/NewItemViewViewModel.swift
@@ -11,4 +11,9 @@ import Foundation
 
 class NewItemViewViewModel: ObservableObject {
     
+    @Published var title = ""
+    @Published var dueDate = Date()
+    @Published var showAlert = false
+    
+    
 }
