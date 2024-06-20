bf47faece3f455bf71fcb8e34a6d78e8d9c9a746
Hoja Arzanesh <ha1797@rit.edu>
2024-06-19 23:31:27 -0400

    add canSave() function
    checks if the entered title, date for task is not empty, not in past

--- ToDoList/ViewModel/NewItemViewViewModel.swift
+++ ToDoList/ViewModel/NewItemViewViewModel.swift
@@ -15,5 +15,15 @@ class NewItemViewViewModel: ObservableObject {
     @Published var dueDate = Date()
     @Published var showAlert = false
     
-    
+    func canSave() -> Bool {
+        guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
+        
+        guard dueDate >= Date().addingTimeInterval(-86400) else {
+            return false
+        }
+        
+        return true
+    }
 }
