a54a131a35ef61263d4c175315b11fb22295e9f9
Hoja Arzanesh <ha1797@rit.edu>
2024-06-20 00:20:52 -0400

    add form for title, and due date of task
    commented out tl-button code until required code becomes available

--- ToDoList/View/NewItemView.swift
+++ ToDoList/View/NewItemView.swift
@@ -19,7 +19,31 @@ struct NewItemView: View {
                 .bold()
                 .padding(.top, 100)
             
-            
+            Form {
+                // title
+                TextField("Title", text: $viewModel.title)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                // due date
+                DatePicker("Due Date", selection: $viewModel.dueDate)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                
+                // button
+//                TLButton(title: "Save", backgroundColor: .pink) {
+//                    if viewModel.canSave() {
+//                        viewModel.save()
+//                        newItemPresented = false
+//                    } else {
+//                        viewModel.showAlert = true
+//                    }
+//                }
+//                .padding()
+            }
+            .alert(isPresented: $viewModel.showAlert) {
+                Alert(title: Text("Error"),
+                      message: Text("Please fill in all fields and select a due date that is today or never.")
+                )
+            }
         }
     }
 }
