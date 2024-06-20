5b8541bcd48c3ac30d7f6bbd7d2beb733969127c
Hoja Arzanesh <ha1797@rit.edu>
2024-06-20 00:08:11 -0400

    add vars viewModel, newItemPresented
    viewModel will be state object of NewItemViewViewModel, and newItemPresented will be binding of it and the var given to NewItemView

--- ToDoList/View/NewItemView.swift
+++ ToDoList/View/NewItemView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct NewItemView: View {
+    
+    @StateObject var viewModel = NewItemViewViewModel()
+    @Binding var newItemPresented: Bool
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
