826191b7b28f2fa7c68d9ce963bae6f1ffbe9298
Hoja Arzanesh <ha1797@rit.edu>
2024-06-20 13:44:27 -0400

    add StateObject viewModel of type RegisterViewViewModel

--- ToDoList/View/RegisterView.swift
+++ ToDoList/View/RegisterView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct RegisterView: View {
+    
+    @StateObject var viewModel = RegisterViewViewModel()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
