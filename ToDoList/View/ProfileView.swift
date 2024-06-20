3640781942676c2e75b4439ec5790ab0d807cc18
Hoja Arzanesh <ha1797@rit.edu>
2024-06-20 13:17:01 -0400

    add profile function taking user
    will uncomment function when User model becomes available

--- ToDoList/View/ProfileView.swift
+++ ToDoList/View/ProfileView.swift
@@ -14,6 +14,49 @@ struct ProfileView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+    
+//    @ViewBuilder
+//    func profile(user: User) -< some View {
+//        
+//        // Avatar
+//        Image(systemName: "person.circle")
+//            .resizable()
+//            .aspectRatio(contentMode: .fit)
+//            .foregroundStyle(Color.blue)
+//            .frame(width: 125, height: 125)
+//            .padding()
+//        
+//        // Info: Name, Email, Member since
+//        VStack(alignment: .leading) {
+//            HStack {
+//                Text("Name: ")
+//                    .bold()
+//                Text(user.name)
+//            }
+//            .padding()
+//            
+//            HStack {
+//                Text("Email: ")
+//                    .bold()
+//                Text(user.email)
+//            }
+//            .padding()
+//            
+//            HStack {
+//                Text("Member Since: ")
+//                    .bold()
+//                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+//            }
+//            .padding()
+//            
+//            Button("Log Out") {
+//                viewModel.logOut()
+//            }
+//            .tint(.red)
+//            
+//            Spacer()
+//        }
+//    }
 }
 
 #Preview {
