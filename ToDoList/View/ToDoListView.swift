1c04126b52a72c42fa16079d8346d3a26f73044f
Hoja Arzanesh <ha1797@rit.edu>
2024-06-20 14:23:29 -0400

    import FirebaseFirestoreSwift, add StateObject, FirestoreQuery vars
    items helps get the tasks from the database, commented out items because ToDoListItem model not available yet

--- ToDoList/View/ToDoListView.swift
+++ ToDoList/View/ToDoListView.swift
@@ -5,14 +5,19 @@
 //  Created by Hoja Arzanesh on 6/17/24.
 //
 
+import FirebaseFirestoreSwift
 import SwiftUI
 
 struct ToDoListView: View {
+    
+    @StateObject var viewModel: ToDoListViewViewModel
+//    @FirestoreQuery var items: [ToDoListItem]
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ToDoListView()
+    ToDoListView(userId: "ty3785kbv3ZtltmRf4KYPh9hnzL2")
 }
