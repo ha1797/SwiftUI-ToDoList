b6654a938c28fe7435a58407945e3156559efefc
Hoja Arzanesh <ha1797@rit.edu>
2024-06-19 23:38:28 -0400

    add save() function
    saves entered task by user to the firebase db. code involving model will be uncommented when model code available.

--- ToDoList/ViewModel/NewItemViewViewModel.swift
+++ ToDoList/ViewModel/NewItemViewViewModel.swift
@@ -17,6 +17,26 @@ class NewItemViewViewModel: ObservableObject {
     
     init() {}
     
+    func save() {
+        
+        // get current user id
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        // create model
+        let newId = UUID().uuidString
+//        let newModel = ToDoListApp(id: newId, title: title, dueDate: dueDate.timeIntervalSince1970, createdDate: Date().timeIntervalSince1970, isDone: false)
+        
+        // save model
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+//            .setData(newModel.asDictionary())
+    }
+    
     func canSave() -> Bool {
         guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
             return false
