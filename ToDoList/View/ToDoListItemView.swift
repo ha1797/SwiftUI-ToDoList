925d1df9fba508b7e600ac5cc524066910acbe1f
Hoja Arzanesh <ha1797@rit.edu>
2024-06-21 10:13:36 -0400

    uncommented remaining code

--- ToDoList/View/ToDoListItemView.swift
+++ ToDoList/View/ToDoListItemView.swift
@@ -15,20 +15,20 @@ struct ToDoListItemView: View {
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
-//                Text(item.title)
-//                    .font(.body)
-//                Text("\(Date(timeIntervalSince1970: item.dueDate).formatted(date: .abbreviated, time: .shortened))")
-//                    .font(.footnote)
-//                    .foregroundStyle(Color(.secondaryLabel))
+                Text(item.title)
+                    .font(.body)
+                Text("\(Date(timeIntervalSince1970: item.dueDate).formatted(date: .abbreviated, time: .shortened))")
+                    .font(.footnote)
+                    .foregroundStyle(Color(.secondaryLabel))
             }
             
             Spacer()
             
             Button(action: {
-//                viewModel.toggleIsDone(item: item)
+                viewModel.toggleIsDone(item: item)
             }, label: {
-//                Image(systemName: item.isDone ? "checkmark.circle.fill" : "circle")
-//                    .foregroundStyle(Color.blue)
+                Image(systemName: item.isDone ? "checkmark.circle.fill" : "circle")
+                    .foregroundStyle(Color.blue)
             })
         }
     }
@@ -36,12 +36,12 @@ struct ToDoListItemView: View {
 
 #Preview {
     ToDoListItemView(
-//        item: .init(
-//            id: "123",
-//            title: "Get milk",
-//            dueDate: Date().timeIntervalSince1970,
-//            createdDate: Date().timeIntervalSince1970,
-//            isDone: false
-//        )
+        item: .init(
+            id: "123",
+            title: "Get milk",
+            dueDate: Date().timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isDone: false
+        )
     )
 }
