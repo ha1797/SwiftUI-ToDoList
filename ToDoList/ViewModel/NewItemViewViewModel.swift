8b3a054f5aa4c6c716538d4c9a902c5bd519b392
Hoja Arzanesh <78914545+ha1797@users.noreply.github.com>
2024-06-21 14:58:40 -0400

    New item view (#11)
    * imported necessary modules, created NewItemViewViewModel class skeleton
    
    * add title, dueDate, showAlert properties
    
    title, dueDate is name and due date of new Task, and showAlert will determine if alert should be shown. Any changes will be notified to observers
    
    * add canSave() function
    
    checks if the entered title, date for task is not empty, not in past
    
    * add init function
    
    * add save() function
    
    saves entered task by user to the firebase db. code involving model will be uncommented when model code available.
    
    * add vars viewModel, newItemPresented
    
    viewModel will be state object of NewItemViewViewModel, and newItemPresented will be binding of it and the var given to NewItemView
    
    * refactored preview to have newItemPresented parameter
    
    * add 'New Item' text
    
    * add form for title, and due date of task
    
    commented out tl-button code until required code becomes available
    
    * changed typo 'ToDoListApp' to 'ToDoListItem'
    
    * uncommented tl-button code
    
    * uncommented code involving ToDoListItem

--- ToDoList/ViewModel/NewItemViewViewModel.swift
+++ ToDoList/ViewModel/NewItemViewViewModel.swift
@@ -5,4 +5,47 @@
 //  Created by Hoja Arzanesh on 6/17/24.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
+
+class NewItemViewViewModel: ObservableObject {
+    
+    @Published var title = ""
+    @Published var dueDate = Date()
+    @Published var showAlert = false
+    
+    init() {}
+    
+    func save() {
+        
+        // get current user id
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        // create model
+        let newId = UUID().uuidString
+        let newModel = ToDoListItem(id: newId, title: title, dueDate: dueDate.timeIntervalSince1970, createdDate: Date().timeIntervalSince1970, isDone: false)
+        
+        // save model
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newModel.asDictionary())
+    }
+    
+    func canSave() -> Bool {
+        guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
+        
+        guard dueDate >= Date().addingTimeInterval(-86400) else {
+            return false
+        }
+        
+        return true
+    }
+}
