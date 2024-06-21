2a3ddb9590b00243775382364bfead3572098cff
Hoja Arzanesh <78914545+ha1797@users.noreply.github.com>
2024-06-21 15:02:43 -0400

    To do list view (#13)
    * imported FirebaseFirestore and created class ToDoListViewViewModel conforming to ObservableObject protocol to ToDoListViewViewModel
    
    * added @Published var showingNewItemView initially set to false and userId constant to ToDoListViewViewModel, which tells whether the new item view is showing or not, and the id of the current user
    
    * added init function to ToDoListViewViewModel, which sets private userId constant to given userId
    
    * add delete func, and commented it out
    
    deletes a given task from the firebase database; commented out because ToDoListItem model not available right now
    
    * import FirebaseFirestoreSwift, add StateObject, FirestoreQuery vars
    
    items helps get the tasks from the database, commented out items because ToDoListItem model not available yet
    
    * add init function
    
    commented out code involving FirestoreQuery
    
    * completed body var, has commented code
    
    displays the tasks gotten from db; commented code involves FirestoreQuery and NewItemView
    
    * uncommented delete function
    
    * uncommented FirestoryQuery items property
    
    * uncommented items initialization
    
    * uncommented code involving ToDoListItemView
    
    * uncommented NewItemView code

--- ToDoList/View/ToDoListView.swift
+++ ToDoList/View/ToDoListView.swift
@@ -5,14 +5,49 @@
 //  Created by Hoja Arzanesh on 6/17/24.
 //
 
+import FirebaseFirestoreSwift
 import SwiftUI
 
 struct ToDoListView: View {
+    
+    @StateObject var viewModel: ToDoListViewViewModel
+    @FirestoreQuery var items: [ToDoListItem]
+    
+    init(userId: String) {
+        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
+        self._viewModel = StateObject(wrappedValue: ToDoListViewViewModel(userId: userId))
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                List(items) { item in
+                    ToDoListItemView(item: item)
+                        .swipeActions(
+                            content: {
+                                Button("Delete") {
+                                    viewModel.delete(item: item)
+                                }
+                                .tint(.red)
+                            })
+                }
+                .listStyle(PlainListStyle())
+            }
+            .navigationTitle("To Do List")
+            .toolbar {
+                Button {
+                    viewModel.showingNewItemView = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $viewModel.showingNewItemView) {
+                NewItemView(newItemPresented: $viewModel.showingNewItemView)
+            }
+        }
     }
 }
 
 #Preview {
-    ToDoListView()
+    ToDoListView(userId: "ty3785kbv3ZtltmRf4KYPh9hnzL2")
 }
--- ToDoList/ViewModel/ToDoListViewViewModel.swift
+++ ToDoList/ViewModel/ToDoListViewViewModel.swift
@@ -5,4 +5,25 @@
 //  Created by Hoja Arzanesh on 6/17/24.
 //
 
+import FirebaseFirestore
 import Foundation
+
+class ToDoListViewViewModel: ObservableObject {
+    
+    @Published var showingNewItemView = false
+    private let userId: String
+    
+    init(userId: String) {
+        self.userId = userId
+    }
+    
+    func delete(item: ToDoListItem) {
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(userId)
+            .collection("todos")
+            .document(item.id)
+            .delete()
+    }
+}
