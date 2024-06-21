83349f70e6e46f3be9f3658eec9bcc526cbacb4c
Hoja Arzanesh <ha1797@rit.edu>
2024-06-21 10:10:37 -0400

    uncommented toggleIsDone func and fixed typo
    asDictionary func call for itemCopy had missing (), included it

--- ToDoList/ViewModel/ToDoListItemViewViewModel.swift
+++ ToDoList/ViewModel/ToDoListItemViewViewModel.swift
@@ -13,19 +13,19 @@ class ToDoListItemViewViewModel: ObservableObject {
     
     init() {}
     
-//    func toggleIsDone(item: ToDoListItem) {
-//        var itemCopy = item
-//        itemCopy.setDone(!item.isDone)
-//        
-//        guard let uId = Auth.auth().currentUser?.uid else {
-//            return
-//        }
-//        
-//        let db = Firestore.firestore()
-//        db.collection("users")
-//            .document(uId)
-//            .collection("todos")
-//            .document(itemCopy.id)
-//            .setData(itemCopy.asDictionary)
-//    }
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
 }
